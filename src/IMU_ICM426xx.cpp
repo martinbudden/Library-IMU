9b636d1a8369207a8593367bb14ee64767fd5ac0
Martin Budden <mjbudden@gmail.com>
2025-07-02 17:06:50 +0100

    Placeholder for IMU_ICM426xx

---
+++ src/IMU_ICM426xx.cpp
@@ -0,0 +1,398 @@
+#include "IMU_ICM426xx.h"
+//#define SERIAL_OUTPUT
+#if defined(SERIAL_OUTPUT)
+#if defined(USE_ARDUINO_ESP32) || defined(ESP32) || defined(ARDUINO_ARCH_ESP32)// ESP32, ARDUINO_ARCH_ESP32 defined in platform.txt
+#include <HardwareSerial.h>
+#else
+#include <Arduino.h>
+#endif
+#endif
+#include <cassert>
+
+namespace { // use anonymous namespace to make items local to this translation unit
+
+constexpr uint8_t REG_SENSOR_CONFIG0        = 0x03;
+constexpr uint8_t REG_GYRO_CONFIG_STATIC2   = 0x0B;
+constexpr uint8_t REG_GYRO_CONFIG_STATIC3   = 0x0C;
+constexpr uint8_t REG_GYRO_CONFIG_STATIC4   = 0x0D;
+constexpr uint8_t REG_GYRO_CONFIG_STATIC5   = 0x0E;
+constexpr uint8_t REG_GYRO_CONFIG_STATIC6   = 0x0F;
+constexpr uint8_t REG_GYRO_CONFIG_STATIC7   = 0x10;
+constexpr uint8_t REG_GYRO_CONFIG_STATIC8   = 0x11;
+constexpr uint8_t REG_GYRO_CONFIG_STATIC9   = 0x12;
+constexpr uint8_t REG_GYRO_CONFIG_STATIC10  = 0x13;
+
+constexpr uint8_t REG_INT_CONFIG            = 0x14;
+constexpr uint8_t REG_FIFO_CONFIG           = 0x16;
+
+constexpr uint8_t REG_TEMP_DATA1            = 0x1D;
+constexpr uint8_t REG_TEMP_DATA0            = 0x1E;
+constexpr uint8_t REG_ACCEL_DATA_X1         = 0x1F;
+constexpr uint8_t REG_ACCEL_DATA_X0         = 0x20;
+constexpr uint8_t REG_ACCEL_DATA_Y1         = 0x21;
+constexpr uint8_t REG_ACCEL_DATA_Y0         = 0x22;
+constexpr uint8_t REG_ACCEL_DATA_Z1         = 0x23;
+constexpr uint8_t REG_ACCEL_DATA_Z0         = 0x24;
+constexpr uint8_t REG_GYRO_DATA_X1          = 0x25;
+constexpr uint8_t REG_GYRO_DATA_X0          = 0x26;
+constexpr uint8_t REG_GYRO_DATA_Y1          = 0x27;
+constexpr uint8_t REG_GYRO_DATA_Y0          = 0x28;
+constexpr uint8_t REG_GYRO_DATA_Z1          = 0x29;
+constexpr uint8_t REG_GYRO_DATA_Z0          = 0x2A;
+
+constexpr uint8_t REG_TMST_FSYNCH           = 0x2B;
+constexpr uint8_t REG_TMST_FSYNCHL          = 0x2C;
+constexpr uint8_t REG_INT_STATUS            = 0x2D;
+
+constexpr uint8_t REG_FIFO_COUNTH           = 0x2E;
+constexpr uint8_t REG_FIFO_COUNTL           = 0x2F;
+constexpr uint8_t REG_FIFO_DATA             = 0x30;
+
+constexpr uint8_t REG_APEX_DATA0            = 0x31;
+constexpr uint8_t REG_APEX_DATA1            = 0x33;
+constexpr uint8_t REG_APEX_DATA2            = 0x34;
+constexpr uint8_t REG_APEX_DATA3            = 0x34;
+constexpr uint8_t REG_APEX_DATA4            = 0x35;
+constexpr uint8_t REG_APEX_DATA5            = 0x36;
+
+constexpr uint8_t REG_INT_STATUS2           = 0x37;
+constexpr uint8_t REG_INT_STATUS3           = 0x38;
+
+constexpr uint8_t REG_SIGNAL_PATH_RESET     = 0x4B;
+constexpr uint8_t REG_INTF_CONFIG0          = 0x4C;
+constexpr uint8_t REG_INTF_CONFIG1          = 0x4D;
+
+constexpr uint8_t REG_PWR_MGMT0             = 0x4E;
+    constexpr uint8_t PWR_OFF               = 0x00;
+    constexpr uint8_t PWR_TEMP_ENABLED      = 0b00000000;
+    constexpr uint8_t PWR_GYRO_LOW_NOISE    = 0b00001100;
+    constexpr uint8_t PWR_ACCEL_LOW_NOISE   = 0b00000011;
+
+constexpr uint8_t REG_GYRO_CONFIG0          = 0x4F;
+constexpr uint8_t REG_ACCEL_CONFIG0         = 0x50;
+constexpr uint8_t REG_GYRO_CONFIG1          = 0x51;
+constexpr uint8_t REG_GYRO_ACCEL_CONFIG0    = 0x52;
+constexpr uint8_t REG_ACCEL_CONFIG1         = 0x53;
+
+constexpr uint8_t REG_TMST_CONFIG           = 0x54;
+constexpr uint8_t REG_APEX_CONFIG           = 0x56;
+constexpr uint8_t REG_SMD_CONFIG            = 0x57;
+constexpr uint8_t REG_FIFO_CONFIG1          = 0x5F;
+constexpr uint8_t REG_FIFO_CONFIG2          = 0x60;
+constexpr uint8_t REG_FIFO_CONFIG3          = 0x61;
+constexpr uint8_t REG_FSYNC_CONFIG          = 0x62;
+constexpr uint8_t REG_INT_CONFIG0           = 0x63;
+constexpr uint8_t REG_INT_CONFIG1           = 0x64;
+
+constexpr uint8_t REG_INT_SOURCE0           = 0x65;
+constexpr uint8_t REG_INT_SOURCE1           = 0x66;
+constexpr uint8_t REG_INT_SOURCE3           = 0x68;
+constexpr uint8_t REG_INT_SOURCE4           = 0x69;
+
+constexpr uint8_t REG_FIFO_LOST_PKT0        = 0x6C;
+constexpr uint8_t REG_FIFO_LOST_PKT1        = 0x6D;
+constexpr uint8_t REG_SELF_TEST_CONFIG3     = 0x70;
+
+constexpr uint8_t REG_WHO_AM_I              = 0x75;
+    constexpr uint8_t WHO_AM_I_RESPONSE_ICM42605  = 0x43;
+    constexpr uint8_t WHO_AM_I_RESPONSE_ICM42688P = 0x47;
+
+constexpr uint8_t REG_BANK_SEL              = 0x76;
+constexpr uint8_t REG_INTF_CONFIG4          = 0x7A;
+constexpr uint8_t REG_INTF_CONFIG5          = 0x7B;
+constexpr uint8_t REG_INTF_CONFIG6          = 0x7C;
+
+// User Bank 2 Register Map
+constexpr uint8_t REG_BANK2_ACCEL_CONFIG_STATIC2    = 0x03;
+constexpr uint8_t REG_BANK2_ACCEL_CONFIG_STATIC3    = 0x04;
+constexpr uint8_t REG_BANK2_ACCEL_CONFIG_STATIC4    = 0x05;
+
+} // end namespace
+
+// NOLINTBEGIN(cppcoreguidelines-pro-type-union-access,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+
+/*!
+Gyroscope data rates up to 6.4 kHz, accelerometer up to 1.6 kHz
+*/
+#if defined(USE_IMU_ICM426xx_SPI)
+IMU_ICM426xx::IMU_ICM426xx(axis_order_e axisOrder, uint32_t frequency, BUS_SPI::spi_index_e SPI_index, const BUS_SPI::pins_t& pins) :
+    IMU_Base(axisOrder, _bus),
+    _bus(frequency, SPI_index, pins)
+{
+    static_assert(sizeof(mems_sensor_data_t) == mems_sensor_data_t::DATA_SIZE);
+    static_assert(sizeof(acc_gyro_data_t) == acc_gyro_data_t::DATA_SIZE);
+    _dmaSpiRegister = REG_OUTX_L_G | BUS_SPI::READ_BIT;
+}
+#else
+IMU_ICM426xx::IMU_ICM426xx(axis_order_e axisOrder, BUS_I2C::i2c_index_e I2C_index, const BUS_I2C::pins_t& pins, uint8_t I2C_address) :
+    IMU_Base(axisOrder, _bus),
+    _bus(I2C_address, I2C_index, pins)
+{
+}
+#if !defined(FRAMEWORK_RPI_PICO) && !defined(FRAMEWORK_ESPIDF) && !defined(FRAMEWORK_TEST)
+IMU_ICM426xx::IMU_ICM426xx(axis_order_e axisOrder, TwoWire& wire, const BUS_I2C::pins_t& pins, uint8_t I2C_address) :
+    IMU_Base(axisOrder, _bus),
+    _bus(I2C_address, wire, pins)
+{
+}
+#endif
+#endif
+
+int IMU_ICM426xx::init(uint32_t targetOutputDataRateHz, gyro_sensitivity_e gyroSensitivity, acc_sensitivity_e accSensitivity, void* i2cMutex) // NOLINT(readability-function-cognitive-complexity)
+{
+    (void)targetOutputDataRateHz;
+    (void)gyroSensitivity;
+    (void)accSensitivity;
+#if defined(I2C_MUTEX_REQUIRED)
+    _i2cMutex = static_cast<SemaphoreHandle_t>(i2cMutex);
+#else
+    (void)i2cMutex;
+#endif
+    static_assert(sizeof(mems_sensor_data_t) == mems_sensor_data_t::DATA_SIZE);
+    static_assert(sizeof(acc_gyro_data_t) == acc_gyro_data_t::DATA_SIZE);
+
+    // MSP compatible gyro and acc identifiers
+    enum { MSP_GYRO_ID_ICM42605 = 14, MSP_GYRO_ID_ICM42688P = 15 };
+    enum { MSP_ACC_ID_ICM42605 = 15, MSP_ACC_ID_ICM42688P = 16 };
+
+    _gyroIdMSP = MSP_GYRO_ID_ICM42605;
+    _accIdMSP = MSP_ACC_ID_ICM42605;
+
+    _bus.setDeviceRegister(REG_ACCEL_DATA_X1, reinterpret_cast<uint8_t*>(&_spiAccGyroData), sizeof(_spiAccGyroData));
+
+    _bus.writeRegister(REG_BANK_SEL, 0); // software reset
+    _bus.writeRegister(REG_PWR_MGMT0, PWR_OFF); // turn off IMU
+
+    // Configure gyro and acc UI Filters
+    _bus.writeRegister(REG_BANK_SEL, 0);
+
+    constexpr uint8_t ACCEL_FILTER_LOW_LATENCY  = 0b11110000;
+    constexpr uint8_t GYRO_FILTER_LOW_LATENCY   = 0b00001111;
+    _bus.writeRegister(REG_GYRO_ACCEL_CONFIG0, ACCEL_FILTER_LOW_LATENCY | GYRO_FILTER_LOW_LATENCY);
+
+    // Configure interrupt pin
+    //constexpr uint8_t INT1_MODE_LATCHED             = 0b00000100;
+    constexpr uint8_t INT1_MODE_PULSED              = 0b00000000;
+    constexpr uint8_t INT1_DRIVE_CIRCUIT_PUSH_PULL  = 0b00000010;
+    constexpr uint8_t INT1_POLARITY_ACTIVE_HIGH     = 0b00000001;
+    _bus.writeRegister(REG_INT_CONFIG, INT1_MODE_PULSED | INT1_DRIVE_CIRCUIT_PUSH_PULL | INT1_POLARITY_ACTIVE_HIGH);
+    constexpr uint8_t INT_CLEAR_ON_STATUS_BIT_READ = 0b00000000;
+    _bus.writeRegister(REG_INT_CONFIG0, INT_CLEAR_ON_STATUS_BIT_READ);
+
+    constexpr uint8_t INT1_UI_DATA_READY_ENABLED     = 0b00001000;
+    _bus.writeRegister(REG_INT_SOURCE0, INT1_UI_DATA_READY_ENABLED);
+
+    // Turn on gyro and acc on again so ODR and FSR can be configured
+    _bus.writeRegister(REG_PWR_MGMT0, PWR_TEMP_ENABLED | PWR_GYRO_LOW_NOISE | PWR_ACCEL_LOW_NOISE);
+    delayMs(1);
+
+    return 0;
+}
+
+IMU_Base::xyz_int32_t IMU_ICM426xx::readGyroRaw()
+{
+    mems_sensor_data_t gyro; // NOLINT(cppcoreguidelines-pro-type-member-init,hicpp-member-init,misc-const-correctness)
+
+    i2cSemaphoreTake();
+    _bus.readRegister(REG_GYRO_DATA_X1, &gyro.data[0], sizeof(gyro));
+    i2cSemaphoreGive();
+
+    return xyz_int32_t {
+        .x = gyro.value.x,
+        .y = gyro.value.y,
+        .z = gyro.value.z
+    };
+}
+
+IMU_Base::xyz_int32_t IMU_ICM426xx::readAccRaw()
+{
+    mems_sensor_data_t acc; // NOLINT(cppcoreguidelines-pro-type-member-init,hicpp-member-init,misc-const-correctness)
+
+    i2cSemaphoreTake();
+    _bus.readRegister(REG_ACCEL_DATA_X1, &acc.data[0], sizeof(acc));
+    i2cSemaphoreGive();
+
+    return xyz_int32_t {
+        .x = acc.value.x,
+        .y = acc.value.y,
+        .z = acc.value.z
+    };
+}
+
+IRAM_ATTR IMU_Base::accGyroRPS_t IMU_ICM426xx::readAccGyroRPS()
+{
+    i2cSemaphoreTake();
+    //_bus.readRegister(REG_OUTX_L_G, &_spiAccGyroData.accGyro.data[0], sizeof(_spiAccGyroData.accGyro));
+    _bus.readDeviceRegister();
+    //_bus.readDeviceRegisterDMA();
+    i2cSemaphoreGive();
+
+    return accGyroRPSFromRaw(_spiAccGyroData.accGyro.value);
+}
+
+void IMU_ICM426xx::setInterruptDriven()
+{
+    _bus.setInterruptDriven();
+}
+
+/*!
+Return the gyroAcc data that was read in the ISR
+*/
+IRAM_ATTR IMU_Base::accGyroRPS_t IMU_ICM426xx::getAccGyroRPS() const
+{
+    return accGyroRPSFromRaw(_spiAccGyroData.accGyro.value);
+}
+
+IMU_Base::accGyroRPS_t IMU_ICM426xx::accGyroRPSFromRaw(const acc_gyro_data_t::value_t& data) const
+{
+#if defined(IMU_BUILD_XPOS_YPOS_ZPOS)
+    return accGyroRPS_t {
+        .gyroRPS = {
+            .x =  static_cast<float>(data.gyro_x - _gyroOffset.x) * _gyroResolutionRPS,
+            .y =  static_cast<float>(data.gyro_y - _gyroOffset.y) * _gyroResolutionRPS,
+            .z =  static_cast<float>(data.gyro_z - _gyroOffset.z) * _gyroResolutionRPS
+        },
+        .acc = {
+            .x =  static_cast<float>(data.acc_x - _accOffset.x)* _accResolution,
+            .y =  static_cast<float>(data.acc_y - _accOffset.y)* _accResolution,
+            .z =  static_cast<float>(data.acc_z - _accOffset.z)* _accResolution
+        }
+    };
+#elif defined(IMU_BUILD_YPOS_XNEG_ZPOS)
+    return accGyroRPS_t {
+        .gyroRPS = {
+            .x =  static_cast<float>(data.gyro_y - _gyroOffset.y) * _gyroResolutionRPS,
+            .y = -static_cast<float>(data.gyro_x - _gyroOffset.x) * _gyroResolutionRPS,
+            .z =  static_cast<float>(data.gyro_z - _gyroOffset.z) * _gyroResolutionRPS
+        },
+        .acc = {
+            .x =  static_cast<float>(data.acc_y - _accOffset.y)* _accResolution,
+            .y = -static_cast<float>(data.acc_x - _accOffset.x)* _accResolution,
+            .z =  static_cast<float>(data.acc_z - _accOffset.z)* _accResolution
+        }
+    };
+#elif defined(IMU_BUILD_XNEG_YNEG_ZPOS)
+    return accGyroRPS_t {
+        .gyroRPS = {
+            .x = -static_cast<float>(data.gyro_x - _gyroOffset.x) * _gyroResolutionRPS,
+            .y = -static_cast<float>(data.gyro_y - _gyroOffset.y) * _gyroResolutionRPS,
+            .z =  static_cast<float>(data.gyro_z - _gyroOffset.z) * _gyroResolutionRPS
+        },
+        .acc = {
+            .x = -static_cast<float>(data.acc_x - _accOffset.x)* _accResolution,
+            .y = -static_cast<float>(data.acc_y - _accOffset.y)* _accResolution,
+            .z =  static_cast<float>(data.acc_z - _accOffset.z)* _accResolution
+        }
+    };
+#elif defined(IMU_BUILD_YNEG_XPOS_ZPOS)
+    return accGyroRPS_t {
+        .gyroRPS = {
+            .x = -static_cast<float>(data.gyro_y - _gyroOffset.y) * _gyroResolutionRPS,
+            .y =  static_cast<float>(data.gyro_x - _gyroOffset.x) * _gyroResolutionRPS,
+            .z =  static_cast<float>(data.gyro_z - _gyroOffset.z) * _gyroResolutionRPS
+        },
+        .acc = {
+            .x = -static_cast<float>(data.acc_y - _accOffset.y)* _accResolution,
+            .y =  static_cast<float>(data.acc_x - _accOffset.x)* _accResolution,
+            .z =  static_cast<float>(data.acc_z - _accOffset.z)* _accResolution
+        }
+    };
+#elif defined(IMU_BUILD_XPOS_ZPOS_YNEG)
+    return accGyroRPS_t {
+        .gyroRPS = {
+            .x =  static_cast<float>(data.gyro_x - _gyroOffset.x) * _gyroResolutionRPS,
+            .y =  static_cast<float>(data.gyro_z - _gyroOffset.z) * _gyroResolutionRPS,
+            .z = -static_cast<float>(data.gyro_y - _gyroOffset.y) * _gyroResolutionRPS
+        },
+        .acc = {
+            .x =  static_cast<float>(data.acc_x - _accOffset.x)* _accResolution,
+            .y =  static_cast<float>(data.acc_z - _accOffset.z)* _accResolution,
+            .z = -static_cast<float>(data.acc_y - _accOffset.y)* _accResolution
+        }
+    };
+#else
+    // Axis order mapping done at run-time
+    const accGyroRPS_t accGyroRPS {
+        .gyroRPS = {
+            .x =  static_cast<float>(data.gyro_x - _gyroOffset.x) * _gyroResolutionRPS,
+            .y =  static_cast<float>(data.gyro_y - _gyroOffset.y) * _gyroResolutionRPS,
+            .z =  static_cast<float>(data.gyro_z - _gyroOffset.z) * _gyroResolutionRPS
+        },
+        .acc = {
+            .x =  static_cast<float>(data.acc_x - _accOffset.x)* _accResolution,
+            .y =  static_cast<float>(data.acc_y - _accOffset.y)* _accResolution,
+            .z =  static_cast<float>(data.acc_z - _accOffset.z)* _accResolution
+        }
+    };
+    switch (_axisOrder) {
+    case XPOS_YPOS_ZPOS:
+        return accGyroRPS;
+        break;
+    case YNEG_XPOS_ZPOS:
+        return accGyroRPS_t {
+            .gyroRPS = {
+                .x = -accGyroRPS.gyroRPS.y,
+                .y =  accGyroRPS.gyroRPS.x,
+                .z =  accGyroRPS.gyroRPS.z
+            },
+            .acc = {
+                .x = -accGyroRPS.acc.y,
+                .y =  accGyroRPS.acc.x,
+                .z =  accGyroRPS.acc.z
+            }
+        };
+        break;
+    case XNEG_YNEG_ZPOS:
+        return accGyroRPS_t {
+            .gyroRPS = {
+                .x = -accGyroRPS.gyroRPS.x,
+                .y = -accGyroRPS.gyroRPS.y,
+                .z =  accGyroRPS.gyroRPS.z
+            },
+            .acc = {
+                .x = -accGyroRPS.acc.x,
+                .y = -accGyroRPS.acc.y,
+                .z =  accGyroRPS.acc.z
+            }
+        };
+        break;
+    case YPOS_XNEG_ZPOS:
+        return accGyroRPS_t {
+            .gyroRPS = {
+                .x =  accGyroRPS.gyroRPS.y,
+                .y = -accGyroRPS.gyroRPS.x,
+                .z =  accGyroRPS.gyroRPS.z
+            },
+            .acc = {
+                .x =  accGyroRPS.acc.y,
+                .y = -accGyroRPS.acc.x,
+                .z =  accGyroRPS.acc.z
+            }
+        };
+        break;
+    case XPOS_ZPOS_YNEG:
+        return accGyroRPS_t {
+            .gyroRPS = {
+                .x =  accGyroRPS.gyroRPS.x,
+                .y =  accGyroRPS.gyroRPS.z,
+                .z = -accGyroRPS.gyroRPS.y
+            },
+            .acc = {
+                .x = -accGyroRPS.acc.x,
+                .y =  accGyroRPS.acc.z,
+                .z = -accGyroRPS.acc.y
+            }
+        };
+        break;
+    default:
+        return accGyroRPS_t {
+            .gyroRPS = mapAxes(accGyroRPS.gyroRPS),
+            .acc = mapAxes(accGyroRPS.acc)
+        };
+        break;
+    } // end switch
+
+    return accGyroRPS;
+#endif
+}
+// NOLINTEND(cppcoreguidelines-pro-type-union-access,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
